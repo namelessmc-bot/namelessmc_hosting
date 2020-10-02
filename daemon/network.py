8a8e5cf10cc8b1f3a1fe36010efd2071ee9ed886
Derkades <robinslot@hotmail.nl>
2020-10-02 23:42:48 +0200

    fix addr in use error

--- daemon/network.py
+++ daemon/network.py
@@ -1,6 +1,7 @@
 PREFIX = '10.130'
 
 def get_webserver_ip(website_id):
+    website_id += 1 # 0.1 IP address is already used, start from .2
     # convert to 16-bit binary string
     binary = format(website_id, '016b')
     # split to two 8-bit strings and convert back to decimal
@@ -8,4 +9,3 @@ def get_webserver_ip(website_id):
     second = int(binary[8:], 2)
     # format as IP address
     return f'{PREFIX}.{first}.{second}'
-
