cf4c931b40d6d8116e015acd49edf73243b259d1
Derkades <robinslot@hotmail.nl>
2020-10-03 01:13:38 +0200

    allow dot in domains

--- frontend/users/models.py
+++ frontend/users/models.py
@@ -6,7 +6,7 @@ from .utils import pass_gen
 
 def validate_domain(value):
     # TODO there has to be a better way
-    allowed_domain_chars = ['_','-','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
+    allowed_domain_chars = ['_','-','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','.']
     for char in value:
         if char not in allowed_domain_chars:
             raise ValidationError("Invalid domain", params={'value': value})
