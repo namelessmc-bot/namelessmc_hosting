244b7651acd8a36948094b37be04926feccc0372
Derkades <robinslot@hotmail.nl>
2020-10-03 12:16:37 +0200

    Fix all users having the same db password

--- frontend/users/signals.py
+++ frontend/users/signals.py
@@ -2,6 +2,7 @@ from django.db.models.signals import post_save, pre_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Account, Website, Job
+from .utils import pass_gen
 
 @receiver(post_save, sender=User)
 def create_account(sender, instance, created, **_kwargs): # pylint: disable=unused-argument
@@ -17,6 +18,8 @@ def save_account(sender, instance, **_kwargs): # pylint: disable=unused-argument
 @receiver(post_save, sender=Website)
 def save_website(sender, instance, created, **_kwargs): # pylint: disable=unused-argument
     if created:
+        instance.db_password = pass_gen()
+        instance.save()
         Job.objects.create(type=Job.CREATE_WEBSITE, priority=Job.NORMAL, content=instance.id)
     else:
         Job.objects.create(type=Job.UPDATE_WEBSITE, priority=Job.HIGH, content=instance.pk)
