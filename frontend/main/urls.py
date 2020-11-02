e2d50920c12963ed95a35a5838fc70d0c315b523
Derkades <robinslot@hotmail.nl>
2020-11-02 19:20:22 +0100

    Show users and websites count

--- frontend/main/urls.py
+++ frontend/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.views.generic import TemplateView
+from .views import home
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='main/index.html'), name='home'),
+    path('', home, name='home'),
     path('pricing', TemplateView.as_view(template_name='main/pricing.html'), name='pricing'),
     path('privacy', TemplateView.as_view(template_name='main/privacy.html'), name='privacy'),
     path('tos', TemplateView.as_view(template_name='main/tos.html'), name='tos'),
--- frontend/main/views.py
+++ frontend/main/views.py
@@ -1,4 +1,10 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 # from django.http import HttpResponse
-# from users.models import Website
+from users.models import Website, User
 
+def home(request):
+    context = {
+        "count_users": User.objects.count(),
+        "count_websites": Website.objects.count()
+    }
+    return render(request, 'main/index.html', context=context)
