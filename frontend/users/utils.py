9ef06891ba1f27e49fc3500e81102269bed6cbb2
Derkades <robinslot@hotmail.nl>
2020-12-23 11:50:08 +0100

    Remove website limit for superusers

--- frontend/users/utils.py
+++ frontend/users/utils.py
@@ -6,6 +6,9 @@ def is_over_website_limit(user):
     from namelessmc_hosting import settings
     from .models import Website, Account
 
+    if user.is_superuser:
+        return False
+
     user_website_count = len(Website.objects.filter(owner=user))
     num_credit = Account.objects.filter(user=user).first().credit
     website_count_limit = settings.USER_MAX_WEBSITES if num_credit > settings.USER_PAID_MINIMUM_CREDITS else settings.USER_MAX_FREE_WEBSITES
