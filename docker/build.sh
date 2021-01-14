32fa943fee0bc82dd568de971ac0ef5892f2d318
Derkades <robinslot@hotmail.nl>
2021-01-14 14:42:41 +0100

    no need to build phpmyadmin, it is no longer used

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker build -t nlmc_hosting/phpmyadmin phpmyadmin
+# docker build -t nlmc_hosting/phpmyadmin phpmyadmin
 cp -r ../frontend frontend/app
 docker build -t nlmc_hosting/frontend frontend
 rm -r frontend/app
