d5037ab1c09e623fd98cd95a36cdd4b50401d156
Derkades <robinslot@hotmail.nl>
2021-01-15 13:17:27 +0100

    Translate field labels

--- frontend/users/models.py
+++ frontend/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from django.utils.translation import gettext as _
 from django.urls import reverse
 from .utils import pass_gen
 
@@ -12,39 +13,39 @@ def validate_domain(value):
                             '3', '4', '5', '6', '7', '8', '9', '0', '.']
     for char in value:
         if char not in allowed_domain_chars:
-            raise ValidationError("Invalid domain", params={'value': value})
+            raise ValidationError(_('Invalid domain'), params={'value': value})
 
     if 'rs-sys.nl' in value \
         or value in ['namedhosting.com', 'site.namedhosting.com', \
                         'www.namedhosting.com']:
-        raise ValidationError("Invalid domain", params={'value': value})
+        raise ValidationError(_('Invalid domain'), params={'value': value})
 
     if 'www.' in value:
-        raise ValidationError("Don't include www. in your domain", params={'value': value})
+        raise ValidationError(_("Don't include www. in your domain"), params={'value': value})
 
     if not '.' in value:
-        raise ValidationError("Invalid domain", params={'value': value})
+        raise ValidationError(_('Invalid domain'), params={'value': value})
 
 
 class Website(models.Model):
-    domain = models.CharField(max_length=100, unique=True, validators=[validate_domain])
-    use_https = models.BooleanField(default=False)
+    domain = models.CharField(max_length=100, unique=True, validators=[validate_domain], verbose_name=_('Domain'))
+    use_https = models.BooleanField(default=False, verbose_name=_('Use HTTPS?'))
     date_created = models.DateTimeField(auto_now_add=True)
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name=_('Website name'))
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     db_password = models.CharField(max_length=50, default=pass_gen)
     files_password = models.CharField(max_length=50, default=pass_gen)
     versions_choices = [
-        ('v2-pr7', 'v2 pre release 7 (old)'),
-        ('v2-pr8', 'v2 pre release 8 (previous)'),
-        ('v2-pr9', 'v2 pre release 9 (current)'),
-        ('v2-pr9-php8', 'v2 pre release 9 on PHP 8 - TESTING ONLY'),
-        ('dev', 'Development - TESTING ONLY'),
-        ('dev-php8', 'Development on PHP 8 - TESTING ONLY')
+        ('v2-pr7', _('v2 pre release 7 (old)')),
+        ('v2-pr8', _('v2 pre release 8 (previous)')),
+        ('v2-pr9', _('v2 pre release 9 (current)')),
+        ('v2-pr9-php8', _('v2 pre release 9 on PHP 8 - TESTING ONLY')),
+        ('dev', _('Development - TESTING ONLY')),
+        ('dev-php8', _('Development on PHP 8 - TESTING ONLY'))
     ]
-    version = models.CharField(max_length=20, default='v2-pr9', choices=versions_choices)
+    version = models.CharField(max_length=20, default='v2-pr9', choices=versions_choices, verbose_name=_('Version'))
     webserver_ip = models.CharField(max_length=20, default=None, null=True, blank=True)
-    www = models.BooleanField(default=False, verbose_name='Enable www.')
+    www = models.BooleanField(default=False, verbose_name=_('Enable www.'))
 
     def __str__(self):
         return self.domain
@@ -73,11 +74,11 @@ class Job(models.Model):
     DELETE_WEBSITE = 4 # Delete website and all data
 
     JOB_TYPES = [
-        (CREATE_WEBSITE, 'Create website'),
-        (UPDATE_WEBSITE, 'Update website'),
-        (RESET_WEBSITE, 'Reset website'),
+        (CREATE_WEBSITE, _('Create website')),
+        (UPDATE_WEBSITE, _('Update website')),
+        (RESET_WEBSITE, _('Reset website')),
         (RESERVED, 'nothing'),
-        (DELETE_WEBSITE, 'Delete website'),
+        (DELETE_WEBSITE, _('Delete website')),
     ]
 
     CRITICAL = 4
