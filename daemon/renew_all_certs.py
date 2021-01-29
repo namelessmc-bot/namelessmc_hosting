9d6a1f113573aec5483782afada73a28fcf5bd76
Robin <robinslot@hotmail.nl>
2021-01-29 23:19:28 +0100

    print just the id instead of the tuple

--- daemon/renew_all_certs.py
+++ daemon/renew_all_certs.py
@@ -7,7 +7,7 @@ cur.execute("SELECT id FROM users_website WHERE use_https='True'")
 websites = cur.fetchall()
 
 for website in websites:
-    print(f'Adding job for {website}')
+    print(f'Adding job for {website[0]}')
     cur.execute("INSERT INTO jobs (type, priority, content, done, running) VALUES (3, 0, %s, false, false)", website)
 
 cur.close()
