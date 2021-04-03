84018cee5269ae0186cb6093d8814b454354c01b
Robin <robinslot@hotmail.nl>
2021-04-03 21:23:47 +0200

    reduce quota

--- daemon/zfs.py
+++ daemon/zfs.py
@@ -2,14 +2,10 @@ import os
 
 ZFS_ROOT = os.environ['ZFS_ROOT']
 
-ZFS_DATASET_OPTIONS = {
-    'quota', '1G'
-}
-
 def create_website_dataset(website_id):
     dataset_name = f'{ZFS_ROOT}/{website_id}'
     print('Creating dataset ' + dataset_name)
-    os.system(f"zfs create -o quota=5G {ZFS_ROOT}/{website_id}")
+    os.system(f"zfs create -o quota=500M {ZFS_ROOT}/{website_id}")
 
 
 def remove_website_dataset(website_id):
